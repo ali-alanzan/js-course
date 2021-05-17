05cef7d126e1eea4951e3cb2ecc9cf3e3eaf9102
ali-alanzan <52928147+ali-alanzan@users.noreply.github.com>
2021-05-17 15:02:13 +0200

    Add files via upload

---
+++ Section 2/6 - switch case statement/script.js
@@ -0,0 +1,23 @@
+// switch case statement
+var weatherSeason = 'spring'; 
+
+switch ( weatherSeason  ) {
+    case 'winter':
+    // code
+    console.log('Stay at home season');
+    break;
+
+    case 'fall':
+        // code
+        console.log('Welcome to fall season');
+    break;      
+    case 'summer':
+    case 'spring':
+
+        console.log('Go to Sea Season and smill the FLowers');
+        break;
+    default:
+        alert('Unknow Weather Season');
+        break;
+
+}
